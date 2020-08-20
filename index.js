2784ab57af229bf5ad21bd580a7a64cbf90104c7
jorgeAgoiz <jorgeagoiz@gmail.com>
2020-08-20 18:14:47 +0200

    little update
    small changes in index.js and index.html files

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const durationInput = document.querySelector(`#duration`);
 const startButton = document.querySelector(`#start`);
 const pauseButton = document.querySelector(`#pause`);
+const circle = document.querySelector(`circle`);
+
+const perimeter = circle.getAttribute(`r`) * 2 * Math.PI;
+circle.setAttribute(`stroke-dasharray`, perimeter);
 
 const timer = new Timer(durationInput, startButton, pauseButton, {
     onStart(){
