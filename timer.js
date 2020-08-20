9a2382d25511a89beb5afc06961188e3976d4464
jorgeAgoiz <jorgeagoiz@gmail.com>
2020-08-20 22:36:51 +0200

    timer working perfect
    last updated, the timer is working perfect.
    20/08

--- timer.js
+++ timer.js
@@ -16,18 +16,16 @@ class Timer {
 
     start = () => {
         if(this.onStart){
-            this.onStart();
+            this.onStart(this.timeRemaining);
         }
 
         this.tick();
-        this.interval = setInterval(this.tick, 1000);
+        this.interval = setInterval(this.tick, 50);
     }
     pause = () => {
         clearInterval(this.interval);
     }
-    onDurationChange = () => {
-
-    }
+    
     tick = () =>{//Using the get and set methods to learn new features about JS
         if(this.timeRemaining <= 0){
             this.pause();
@@ -35,9 +33,9 @@ class Timer {
                 this.onComplete();
             }
         } else {
-            this.timeRemaining = this.timeRemaining -1;//Very simplify call to set and get method
+            this.timeRemaining = this.timeRemaining -0.05;//Very simplify call to set and get method
             if(this.onTick){
-                this.onTick();
+                this.onTick(this.timeRemaining);
             }
         }
     }
@@ -48,6 +46,6 @@ class Timer {
     }
 
     set timeRemaining(time){//To invoke the set method is not necessary to call with parentheses()
-        this.durationInput.value = time;//To pass the argument is not necessary (argument)
+        this.durationInput.value = time.toFixed(2);//To pass the argument is not necessary (argument)
     }// the value behind the equal symbol automatically passed as argument in the set method
 }
