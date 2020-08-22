720cfd9fd7ce80284bdf8f9c20bd2e3c092d9374
jorgeAgoiz <jorgeagoiz@gmail.com>
2020-08-22 10:29:37 +0200

    Last update  22/08
    Timer App finished

--- index.js
+++ index.js
@@ -3,21 +3,20 @@ const startButton = document.querySelector(`#start`);
 const pauseButton = document.querySelector(`#pause`);
 const circle = document.querySelector(`circle`);
 
-const perimeter = circle.getAttribute(`r`) * 2 * Math.PI;
-circle.setAttribute(`stroke-dasharray`, perimeter);
+const perimeter = circle.getAttribute(`r`) * 2 * Math.PI;//To define the perimeter
+circle.setAttribute(`stroke-dasharray`, perimeter);//Set the attribute with the perimeter
 
 let duration;
-
-
+//Create a timer object
 const timer = new Timer(durationInput, startButton, pauseButton, {
     onStart(totalDuration){
         duration = totalDuration;
     },
-    onTick(timeRemaining){
+    onTick(timeRemaining){//Update the dashoffset style every 5 miles of second
         circle.setAttribute(`stroke-dashoffset`, (perimeter * timeRemaining)/ duration - perimeter);
         
     },
-    onComplete(){
+    onComplete(){//Messages of complete
         console.log('Timer is completed');
         alert('Time is over');
     }
--- timer.js
+++ timer.js
@@ -1,5 +1,7 @@
-// Simple timer in JS
+// Timer in JS
+
 class Timer {
+    //Define constructor
     constructor(durationInput, startButton, pauseButton, callbacks){
         this.durationInput = durationInput;
         this.startButton = startButton;
@@ -9,38 +11,40 @@ class Timer {
             this.onTick = callbacks.onTick;
             this.onComplete = callbacks.onComplete;
         }
-
+        //Add event listener to the pause and start button
         this.startButton.addEventListener('click', this.start);
         this.pauseButton.addEventListener('click', this.pause);
     }
 
     start = () => {
-        if(this.onStart){
+        if(this.onStart){//If this method is true(exist), call it.
             this.onStart(this.timeRemaining);
         }
 
-        this.tick();
+        this.tick();//Call tick method first time and later call every five miles of a second
         this.interval = setInterval(this.tick, 50);
     }
     pause = () => {
-        clearInterval(this.interval);
+        clearInterval(this.interval);//To stop the interval (pause)
     }
     
-    tick = () =>{//Using the get and set methods to learn new features about JS
-        if(this.timeRemaining <= 0){
+    tick = () =>{
+        if(this.timeRemaining <= 0){//Whe time will be zero stops.
             this.pause();
-            if(this.onComplete){
+            if(this.onComplete){//If exists this method, call it.
                 this.onComplete();
             }
         } else {
             this.timeRemaining = this.timeRemaining -0.05;//Very simplify call to set and get method
-            if(this.onTick){
+            if(this.onTick){//If exists this method, call it.
                 this.onTick(this.timeRemaining);
             }
         }
     }
-        
 
+        
+    //Using the get and set methods to learn new features about JS
+    
     get timeRemaining(){//To invoke the get method is not necessary to call with parentheses()
         return parseFloat(this.durationInput.value);
     }
